fc54f00d4110dccdb4951663978f8c7b82a0d823
joeappleton18 <joe@nuprotection.co.uk>
2019-02-06 08:16:30 +0000

    2) consume api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+      <Breweries/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Breweries from './components/Breweries.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Breweries
   }
 }
 </script>
---
+++ src/components/Breweries.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-info">Breweries List</h1>
+            </div>
+            <!-- /.col-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-6 brew-list">
+                <div class="card">
+                    <ul class="list-group list-group-flush">
+                        <li v-bind:key="index" v-for="(brew, index) in breweries" class="list-group-item  brew-item">
+                            {{brew.name}}, {{brew.state}}
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            <!-- /.col-6 -->
+            <div class="col-6">
+
+            </div>
+            <!-- /.col-6 -->
+        </div>
+        <!-- /.row -->
+
+
+    </div>
+
+
+</template>
+
+<script>
+
+    import axios from 'axios';
+
+    export default {
+        name: 'Breweries',
+        data: function () {
+            return {
+                breweries: []
+            }
+        },
+
+        mounted: function () {
+            axios.get('https://api.openbrewerydb.org/breweries').then(r => {
+                this.breweries = r.data;
+            })
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+    .brew-list {
+        overflow-y: scroll;
+        height: 95vh;
+        .brew-item {
+            &:hover {
+                background-color: darkgrey;
+            }
+
+        }
+    }
+
+</style>
